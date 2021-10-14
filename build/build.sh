525c5d65c1ac4dab01ce0761241e67570e57136b
Jakob Eberhardt <jeberhardt@pcepsft100.dyndns.cern.ch>
2021-10-14 15:42:48 +0200

    fix build.sh

--- build/build.sh
+++ build/build.sh
@@ -13,3 +13,8 @@ git clone $REPO_URL
 cd cernvm-five
 buildah bud --format "$OUTPUT_FORMAT" -t "$TAG:$VERSION" -f "docker/$DOCKERFILE.$KIND" . 
 buildah push --format "$OUTPUT_FORMAT" "$TAG:$VERSION" docker-archive:"$BUILD_DIR/$TAG.tar"
+buildah images
+cd $BUILD_DIR
+gzip -k -v "$TAG.tar"
+ls -lah $BUILD_DIR
+cp *.gz ./image_dest
