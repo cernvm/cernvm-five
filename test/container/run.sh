98b331107bac7d274af19a99b5f1299e3b5798fe
Jakob Eberhardt <jeberhardt@pcepsft100.dyndns.cern.ch>
2021-11-01 14:55:08 +0100

    added testrun summary

--- test/container/run.sh
+++ test/container/run.sh
@@ -3,6 +3,9 @@
 logfile=$1
 testsuite=$2
 test_dir=/test
+passed=0
+failed=0
+num_tests=${#testsuite[@]}
 # logger
 log() {
 echo '['$(date +"%D %T %z")'][C]' $1 | tee -a $logfile
@@ -12,8 +15,19 @@ for t in "${testsuite[@]}"
 do
   . ./src/$t/main
   log "Started $cvm_test_name"
-  cvm_run_test 
+  cvm_run_test
+  if [ $? != "0" ]; then
+    log "Test $cvm_test_name FAILED"
+    ((failed++))
+  else 
+    log "Test $cvm_test_name PASSED" 
+    ((passed++))
+   fi
 done
+
 log "Tests done... stopping container"
+log "${passed} of ${num_tests} tests passed"
+log "${failed} of ${num_tests} tests failed"
+
 exit
 
