468c29c2b1d0a1d95ff5a5ec79a2f9e049f3c699
Jakob Eberhardt <jeberhardt@pcepsft100.dyndns.cern.ch>
2021-11-04 08:50:27 +0100

    improved mounting

--- system/common/mount.sh
+++ system/common/mount.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 # This file is part of CernVM 5.
-# Mounting cvmfs
-mkdir -p /cvmfs/sft.cern.ch 
-mount -t cvmfs sft.cern.ch /cvmfs/sft.cern.ch
-
-mkdir -p /cvmfs/fcc.cern.ch
-mount -t cvmfs fcc.cern.ch /cvmfs/fcc.cern.ch
-
-mkdir -p /cvmfs/cernvm-five.cern.ch
-mount -t cvmfs cernvm-five.cern.ch /cvmfs/cernvm-five.cern.ch
-
-mkdir -p /cvmfs/fcc-nightlies.cern.ch
-mount -t cvmfs fcc-nightlies.cern.ch /cvmfs/fcc-nightlies.cern.ch
+# Mounting every repo specified in /etc/cvmfs/default.local 
+. /etc/cvmfs/default.local 
+repos=(${CVMFS_REPOSITORIES//,/ })
+for r in "${repos[@]}"
+do
+  mkdir -p /cvmfs/$r 
+  mount -t cvmfs $r /cvmfs/$r
+done
