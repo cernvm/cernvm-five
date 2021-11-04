f61a8eff05ab8e3571d8def6283df8afb851faae
Jakob Eberhardt <jeberhardt@pcepsft100.dyndns.cern.ch>
2021-11-04 12:04:34 +0100

    added return code

--- system/common/mount.sh
+++ system/common/mount.sh
@@ -3,8 +3,14 @@
 # Mounting every repo specified in /etc/cvmfs/default.local 
 . /etc/cvmfs/default.local 
 repos=(${CVMFS_REPOSITORIES//,/ })
+res=0
 for r in "${repos[@]}"
 do
   mkdir -p /cvmfs/$r 
   mount -t cvmfs $r /cvmfs/$r
+  if [ $? -ne 0 ]; then
+  res=1
+  fi
 done
+
+exit $res
