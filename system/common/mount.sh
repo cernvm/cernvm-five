eab358f03c95c93d0e01f89932c4afad6200892e
Jakob Eberhardt <jeberhardt@pcepsft100.dyndns.cern.ch>
2021-11-08 10:44:57 +0100

    added todo: check for host CernVM FS

--- system/common/mount.sh
+++ system/common/mount.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This file is part of CernVM 5.
 # Mounting every repo specified in /etc/cvmfs/default.local 
+# todo: Check if CernVM FS is already available via host. 
 . /etc/cvmfs/default.local 
 repos=(${CVMFS_REPOSITORIES//,/ })
 res=0
