5568150238df43b1af799e4261c1ec93c9ef3052
Jakob Eberhardt <jeberhardt@pcepsft100.dyndns.cern.ch>
2021-11-08 15:02:03 +0100

    added host as an option

--- system/common/mount.sh
+++ system/common/mount.sh
@@ -2,17 +2,20 @@
 # This file is part of CernVM 5.
 # Mounting every repo specified in /etc/cvmfs/default.local 
 
-# todo: Check if CernVM FS is already available via host. 
-. /etc/cvmfs/default.local 
-repos=(${CVMFS_REPOSITORIES//,/ })
-res=0
-for r in "${repos[@]}"
-do
-  mkdir -p /cvmfs/$r 
-  mount -t cvmfs $r /cvmfs/$r
-  if [ $? -ne 0 ]; then
-  res=1
-  fi
-done
+if ls /cvmfs/cernvm-five.cern.ch 2> /dev/null; then
+  exit 0
+else
+  . /etc/cvmfs/default.local 
+  repos=(${CVMFS_REPOSITORIES//,/ })
+  res=0
+  for r in "${repos[@]}"
+  do
+    mkdir -p /cvmfs/$r 
+    mount -t cvmfs $r /cvmfs/$r
+    if [ $? -ne 0 ]; then
+      res=1
+    fi
+  done
+fi
 
 exit $res
