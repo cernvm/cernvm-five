8a8b1ee3a68ab2f4963aede80d67c39818fb3a77
Jakob Eberhardt <jeberhardt@pcepsft100.dyndns.cern.ch>
2021-11-09 14:38:36 +0100

    added todo

---
+++ system/minimization/minimization.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This file is part of CernVM 5.
+set -e
+# todo: using dnf instead
+# Removing packages
+sudo rpm --root=$BUILD_DIR -e  cracklib-dicts \
+    cracklib \
+    adwaita-icon-theme \
+    adwaita-cursor-theme \
+    gsettings-desktop-schemas --nodeps  
+
+# Removing cache
+sudo rm -r build/var/cache/
+dnf clean all
+
+# Can be used to determine over fetched packages
+# rpm -qia|awk '$1=="Name" { n=$3} $1=="Size" {s=$3*10^-6} $1=="Description" {print s  " " n }' |sort -n 
