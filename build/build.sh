1cd7899d27213988890f0f1f51ee3d4745f7cef5
Jakob Eberhardt <jeberhardt@pcepsft100.dyndns.cern.ch>
2021-11-09 09:13:08 +0100

    changed tagging

--- build/build.sh
+++ build/build.sh
@@ -2,7 +2,7 @@
 ### This file is part of CernVM 5.
 # Used in a build container
 set -e
-NAME="cvm-five"
+NAME="cvmvm"
 DOCKERFILE="Dockerfile"
 TAG="baselayer"
 REPO_URL="https://github.com/cernvm/cernvm-five"
@@ -12,10 +12,10 @@ DEST_DIR="./image_dest"
 
 git clone $REPO_URL
 cd cernvm-five
-buildah bud --format "$OUTPUT_FORMAT" -t "$NAME:$TAG" -f "docker/$DOCKERFILE.$TAG" . 
-buildah push --format "$OUTPUT_FORMAT" "$NAME:$TAG" docker-archive:"$BUILD_DIR/$NAME.tar"
+buildah bud --format "$OUTPUT_FORMAT" -t "$NAME:$VERSION" -f "docker/$DOCKERFILE" . 
+buildah push --format "$OUTPUT_FORMAT" "$NAME:$VERSION" docker-archive:"$BUILD_DIR/$NAME.tar"
 buildah images
 cd $BUILD_DIR
 gzip -k -v "$NAME.tar"
 # move to host mountpoint $DEST_DIR
-mv cvm-five.tar.gz $DEST_DIR/${NAME}_${TAG}.tar.gz
+mv cvm-five.tar.gz $DEST_DIR/${NAME}-${VERSION}.tar.gz
