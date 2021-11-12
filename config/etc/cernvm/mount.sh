663177dc1c21101ccb850e7043612c68d86b33aa
Jakob Eberhardt <jeberhardt@pcepsft100.dyndns.cern.ch>
2021-11-12 10:29:10 +0100

    removed comments

--- config/etc/cernvm/mount.sh
+++ config/etc/cernvm/mount.sh
@@ -20,29 +20,3 @@ else
 fi
 
 exit $res
-
-
-
-
-
-# #!/bin/bash
-# # This file is part of CernVM 5.
-# # Mounting every repo specified in /etc/cvmfs/default.local 
-
-# if [ "$(cvmfs_config status)" != "" ]; then
-#   exit 0
-# fi
-
-#   . /etc/cvmfs/default.local 
-#   repos=("${CVMFS_REPOSITORIES//,/ }")
-#   res=0
-#   for r in "${repos[@]}"
-#   do
-#     mkdir -p /cvmfs/$r 
-#     mount -t cvmfs $r /cvmfs/$r
-#     if [ $? -ne 0 ]; then
-#       res=1
-#     fi
-#   done
-
-# exit $res
