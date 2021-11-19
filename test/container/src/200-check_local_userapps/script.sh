d8be5144816f0a3d35d35c488ab9be11ae36caca
Jakob Eberhardt <jeberhardt@pcepsft100.dyndns.cern.ch>
2021-11-19 11:11:43 +0100

    fixed exit code

--- test/container/src/200-check_local_userapps/script.sh
+++ test/container/src/200-check_local_userapps/script.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-# todo: fix test
-fail="false"
-apps=("nano", "ping")
+fail=
+apps=("nano" "ping")
 for a in "${apps[@]}"
 do
   which $a
-  if [ $? == "0" ]; then
+  if [ $? -eq 0 ]; then
     fail="true"
   fi
 done
  
-if [ $fail == "true" ]; then
+if [[ $fail == "true" ]]; then
     exit 1
 fi
 exit 0
