5be015179219347f7446c035413b5a14d0f9a41d
Jakob Eberhardt <jeberhardt@pcepsft100.dyndns.cern.ch>
2021-11-25 16:44:59 +0100

    added test 500

---
+++ test/container/src/500-check-cvmfs_config_files/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Array files for all checked files in image
+files=("/etc/cernvm/functions" "/etc/cvmfs/config.d/cernvm-five.cern.ch.conf" "/etc/cvmfs/default.d/90-cernvm.conf" "/etc/cvmfs/keys/cernvm-five.cern.ch.pub")
+fail= 
+for f in "${files[@]}"
+do
+  if [ ! -f $f  ]; then
+    fail="true"
+    echo "$f is missing"
+  fi
+done
+
+if [[ $fail == "true" ]]; then
+  exit 1
+fi
+
+exit 0
+
