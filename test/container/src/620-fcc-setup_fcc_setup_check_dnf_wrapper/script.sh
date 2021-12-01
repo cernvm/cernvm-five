8d189754a90825d29f21e0371fa370f8fbfe2720
jakobeberhardt <jakob.eberhardt@mail.de>
2021-12-01 09:19:52 +0100

    added test 620

---
+++ test/container/src/620-fcc-setup_fcc_setup_check_dnf_wrapper/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+. ./etc/cernvm/functions
+cernvm_config mount_cvmfs -a
+cernvm_config setup_platform
+. /cvmfs/sw.hsf.org/spackages4/key4hep-stack/release-2021-10-29-ip7764o/x86_64-centos8-gcc8.4.1-opt/setup.sh
+
+dnf install -y -v --nogpgcheck nautilus firefox --nodocs
+exit $?
+
