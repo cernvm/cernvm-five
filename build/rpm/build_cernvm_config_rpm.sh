c99cd1fec0e3f7139c94167c5603047224c24972
jakobeberhardt <jakob.eberhardt@mail.de>
2021-12-01 16:32:43 +0100

    added rpm build to jenkins ci

--- build/rpm/build_cernvm_config_rpm.sh
+++ build/rpm/build_cernvm_config_rpm.sh
@@ -3,11 +3,17 @@
 # Script is used to build a CernVM 5 config rpm ###
 set -e
 usage() {
-  echo "$0 <rpm nametag> "
+  echo "$0 <rpm nametag> <jenkins-buildnumber (injected)>"
 }
 
 NAME=$1
 if [ -z $NAME ]; then
+  usage
+  exit 1
+fi
+
+BUILD_NUMBER=$2
+if [ -z $BUILD_NUMBER ]; then
   usage
   exit 1
 fi
@@ -21,4 +27,4 @@ rpmbuild -ba cernvm-config.spec
 DEST_DIR="./rpm_dest"
 
 # move rpm to host mountpoint $DEST_DIR 
-mv ~/rpmbuild/RPMS/x86_64/*.rpm $DEST_DIR/${NAME}.rpm
+mv ~/rpmbuild/RPMS/x86_64/*.rpm $DEST_DIR/${NAME}-1.${BUILD_NUMBER}.x86_64.rpm
