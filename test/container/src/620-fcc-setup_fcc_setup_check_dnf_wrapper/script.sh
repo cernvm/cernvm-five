97c3facaf103c6d3856726d95993ac0d7138347f
jakobeberhardt <jakob.eberhardt@mail.de>
2021-12-01 10:00:12 +0100

    fixed script

--- test/container/src/620-fcc-setup_fcc_setup_check_dnf_wrapper/script.sh
+++ test/container/src/620-fcc-setup_fcc_setup_check_dnf_wrapper/script.sh
@@ -3,7 +3,7 @@
 cernvm_config mount_cvmfs -a
 cernvm_config setup_platform
 . /cvmfs/sw.hsf.org/spackages4/key4hep-stack/release-2021-10-29-ip7764o/x86_64-centos8-gcc8.4.1-opt/setup.sh
+wrapped_dnf install -y -v --nogpgcheck nautilus firefox --nodocs
 
-dnf install -y -v --nogpgcheck nautilus firefox --nodocs
 exit $?
 
