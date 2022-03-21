6b99b0ff0b5f583b7bb2bed54728a4d6a5c401f1
jakobeberhardt <jakob.eberhardt@mail.de>
2022-03-21 15:44:00 +0100

    added dnf conf and removed unnecessary options

--- system/install/install_cvmfs.sh
+++ system/install/install_cvmfs.sh
@@ -4,20 +4,20 @@ set -e
 # todo: building a single rpm
 
 # Used for docker
-dnf install --installroot=$BUILD_DIR --releasever=/  --nodocs --setopt=install_weak_deps=False -y libXpm \
+dnf install --installroot=$BUILD_DIR  --releasever=/ --nodocs libXpm \
      xauth \
      PackageKit-gtk3-module  #root: error while loading shared libraries: libXft.so.2: cannot open shared object file: No such file or directory
 
 # HEP_OSlibs
-dnf install -y --installroot=$BUILD_DIR --releasever=/ --nodocs --setopt=install_weak_deps=False https://linuxsoft.cern.ch/wlcg/centos8/x86_64/wlcg-repo-1.0.0-1.el8.noarch.rpm 
-dnf install -y --installroot=$BUILD_DIR --releasever=/ --nodocs --setopt=install_weak_deps=False HEP_OSlibs
+dnf install --installroot=$BUILD_DIR --releasever=/ --nodocs https://linuxsoft.cern.ch/wlcg/centos8/x86_64/wlcg-repo-1.0.0-1.el8.noarch.rpm 
+dnf install --installroot=$BUILD_DIR --releasever=/ --nodocs HEP_OSlibs
 
 # dnf packagemanager
-dnf install -y --installroot=$BUILD_DIR --releasever=/ --nodocs --setopt=install_weak_deps=False dnf
+dnf install --installroot=$BUILD_DIR --releasever=/ --nodocs dnf
 
 # CernVM-FS
-sudo dnf install --installroot=$BUILD_DIR --releasever=/ --nodocs --setopt=install_weak_deps=False  -y https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm  
-sudo dnf install -y --installroot=$BUILD_DIR --releasever=/ --nodocs --setopt=install_weak_deps=False cvmfs 
+dnf install -y --installroot=$BUILD_DIR --releasever=/ --nodocs https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm  
+dnf install -y --installroot=$BUILD_DIR --releasever=/ --nodocs cvmfs 
 
 # CernVM config and helper functions
 # todo: Add RPM version to rolling tag
