0dc7f03eb12e63d5f4dee0dd043195decfcb4747
jakobeberhardt <jakob.eberhardt@mail.de>
2022-04-07 12:29:06 +0200

    added architecture

--- build/container/build_image.sh
+++ build/container/build_image.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 ### This file is part of CernVM 5.
+# For e.g. sudo podman run -it --rm --device /dev/fuse --cap-add SYS_ADMIN -v ~/tmp:/build/image_dest:Z builder bash build_image.sh "cernvm" "5" "docker" "x86_64"
 # Script is used in a Buildah container to build a tagged image
 # Image: https://github.com/containers/buildah
 set -e
 usage() {
-  echo "$0 <image nametag> <image versiontag> <output format e.g. docker> "
+  echo "$0 <image nametag> <image versiontag> <output format e.g. docker> <arch>"
 }
 
 NAME=$1
@@ -25,6 +26,12 @@ if [ -z $OUTPUT_FORMAT ]; then
   exit 1
 fi
 
+ARCH=$4
+if [ -z $ARCH ]; then
+  usage
+  exit 1
+fi
+
 REPO_URL="https://github.com/cernvm/cernvm-five"
 
 # Image is exported to BUILD_DIR_CONTAINER
@@ -33,17 +40,13 @@ BUILD_DIR_CONTAINER="/build"
 # Image is copied to host mountpoint DEST_DIR
 DEST_DIR="./image_dest"
 
-# todo: add rpm package build
-# toso: cleanup dest_dir
-
 git clone $REPO_URL
 cd cernvm-five
-# todo: Buildah via https
-buildah bud --format "$OUTPUT_FORMAT" -t "$NAME:$VERSION" -f "docker/Dockerfile" . 
-buildah push --format "$OUTPUT_FORMAT" "$NAME:$VERSION" docker-archive:"$BUILD_DIR_CONTAINER/$NAME.tar"
+buildah bud --format "$OUTPUT_FORMAT" -t "$NAME:$VERSION.$OUTPUT_FORMAT-$ARCH" -f "docker/Dockerfile" . 
+buildah push --format "$OUTPUT_FORMAT" "$NAME:$VERSION.$OUTPUT_FORMAT-$ARCH" docker-archive:"$BUILD_DIR_CONTAINER/$NAME.tar"
 buildah images
 cd $BUILD_DIR_CONTAINER
 gzip -k -v "$NAME.tar"
 
 # move image to host mountpoint $DEST_DIR as .tar.gz
-mv $NAME.tar.gz $DEST_DIR/${NAME}-${VERSION}.tar.gz
+mv $NAME.tar.gz $DEST_DIR/${NAME}-${VERSION}.${OUTPUT_FORMAT}-${ARCH}.tar.gz
