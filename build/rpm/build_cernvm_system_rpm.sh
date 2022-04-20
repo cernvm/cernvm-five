2f1e1a2855d108703526969b2231ce5397bb9877
jakobeberhardt <jakob.eberhardt@mail.de>
2022-04-20 14:14:57 +0200

    added system rpm

---
+++ build/rpm/build_cernvm_system_rpm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+### This file is part of CernVM 5. ###
+# Script is used to build a CernVM 5 system rpm ###
+set -e
+usage() {
+  echo "$0 <rpm nametag> <jenkins-buildnumber (injected)>"
+}
+
+NAME=$1
+if [ -z $NAME ]; then
+  usage
+  exit 1
+fi
+
+BUILD_NUMBER=$2
+if [ -z $BUILD_NUMBER ]; then
+  usage
+  exit 1
+fi
+
+REPO_URL="https://github.com/cernvm/cernvm-five"
+git clone $REPO_URL
+
+rpmbuild -ba ./rpm/cernvm-system.spec 
+
+DEST_DIR="/build/rpm_dest"
+
+# move rpm to host mountpoint $DEST_DIR 
+mv /root/rpmbuild/RPMS/x86_64/*.rpm $DEST_DIR/${NAME}-1.${BUILD_NUMBER}.$(uname -m).rpm
---
+++ rpm/cernvm-system.spec
@@ -0,0 +1,20 @@
+Summary: CernVM 5 system
+Name: cernvm-system-default
+Version: 1.0.0
+Group: System/Middleware
+Release: 1
+Group: Applications/System
+License: BSD
+
+Requires: cvmfs
+Requires: HEP_OSlibs
+Requires: dnf
+
+%description
+Meta-package including a CernVM-FS Client and HEP_OSlibs for CernVM 5.
+
+%files
+
+%doc
+
+%changelog
