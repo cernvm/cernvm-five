d308464452396d1901eb32eb92c2a4517476dd49
jakobeberhardt <jakob.eberhardt@mail.de>
2022-05-10 14:20:02 +0200

    tmp

--- system/install/install_cvmfs.sh
+++ system/install/install_cvmfs.sh
@@ -3,8 +3,8 @@
 set -e
 
 # Adding wlcg and cvmfs repositories
-dnf install --installroot=$BUILD_DIR --releasever=/ --nodocs https://linuxsoft.cern.ch/wlcg/centos8/x86_64/wlcg-repo-1.0.0-1.el8.noarch.rpm
-dnf install --installroot=$BUILD_DIR --releasever=/ --nodocs https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm  
+dnf install -y --installroot=$BUILD_DIR --releasever=/ --nodocs https://linuxsoft.cern.ch/wlcg/centos8/x86_64/wlcg-repo-1.0.0-1.el8.noarch.rpm
+dnf install -y --installroot=$BUILD_DIR --releasever=/ --nodocs https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm  
 
 # CernVM config and system
 wget -P / http://ecsft.cern.ch/dist/cernvm/five/rpms/latest/cernvm-system-default-latest-x86_64.rpm
