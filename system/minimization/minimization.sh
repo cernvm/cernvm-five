abc7a60e1e47fba66a7da8d1abf797addf49a86d
jakobeberhardt <jakob.eberhardt@mail.de>
2022-05-10 14:53:43 +0200

    add package

--- system/minimization/minimization.sh
+++ system/minimization/minimization.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This file is part of CernVM 5. ###
-# todo: using dnf instead
+# todo: Use automated script
 # Removing packages
-sudo rpm --root=$BUILD_DIR -e  cracklib-dicts \
+sudo rpm --root=$BUILD_DIR -e --nodeps cracklib-dicts \
     cracklib \
     adwaita-icon-theme \
     adwaita-cursor-theme \
@@ -13,11 +13,11 @@ sudo rpm --root=$BUILD_DIR -e  cracklib-dicts \
     tar \
     binutils \
     hwdata \
-    gsettings-desktop-schemas --nodeps  
+    grubby \
+    gsettings-desktop-schemas binutils   
 
 # Removing cache
 sudo rm -r build/var/cache/
-dnf clean all
 
 # Can be used to determine over fetched packages
 # rpm -qia|awk '$1=="Name" { n=$3} $1=="Size" {s=$3*10^-6} $1=="Description" {print s  " " n }' |sort -n 
