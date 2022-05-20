942a5615f3c195dbb67783e0c4c7338abf639359
jakobeberhardt <jakob.eberhardt@mail.de>
2022-05-20 15:51:53 +0200

    add ci for cvmfs meta-package

---
+++ build/rpm/build_cernvm_system_cvmfs_rpm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+### This file is part of CernVM 5. ###
+# Script is used to build a meta-rpm for a CernVM 5 system application area on cvmfs rpm ###
+set -e
+usage() {
+  echo "$0 <rpm nametag> <jenkins-buildnumber (injected)>"
+}
+
+NAME=$1
+if [ -z $NAME ]; then
+  usage
+  exit 1
+fi
+
+BUILD_NUMBER=$2
+if [ -z $BUILD_NUMBER ]; then
+  usage
+  exit 1
+fi
+
+REPO_URL="https://github.com/cernvm/cernvm-five"
+git clone $REPO_URL
+cd /build/cernvm-five
+rpmbuild -ba ./rpm/cernvm-system-cvmfs.spec 
+
+DEST_DIR="/build/rpm_dest"
+
+# move rpm to host mountpoint $DEST_DIR 
+mv /root/rpmbuild/RPMS/$(uname -m)/*.rpm $DEST_DIR/${NAME}-1.${BUILD_NUMBER}.$(uname -m).rpm
