4f480a7d518e51006949497db316ab67555817e2
jakobeberhardt <jakob.eberhardt@mail.de>
2022-06-01 13:13:06 +0200

    drop setup

--- test/container/src/640-fcc-whizard_host/script.sh
+++ test/container/src/640-fcc-whizard_host/script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
-. ./etc/cernvm/functions
-cernvm_config setup_systemapps
 source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
 mkdir -p test_whizard/Z_mumu; cd test_whizard/Z_mumu
 wget https://fccsw.web.cern.ch/fccsw/share/gen/whizard/Zpole/Z_mumu.sin
